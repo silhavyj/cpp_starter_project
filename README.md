696144f6bd1b572123af18c43229eed228b98b39
silhavyj <jakub.sil@seznam.cz>
2022-06-04 09:20:12 +0200

    Added README.md, renamed test executable

---
+++ README.md
@@ -0,0 +1,16 @@
+```
+git clone --recursive https://github.com/silhavyj/cpp_starter_project.git
+```
+
+```
+cmake -S . -B build
+make -C build
+```
+
+```
+./build/src/myproject
+```
+
+```
+./build/test/mytest
+```
--- test/tests1.cpp
+++ test/tests1.cpp
@@ -1,10 +1,6 @@
 #include "my_math.h"
 #include <gtest/gtest.h>
 
-unsigned int Factorial(unsigned int number) {
-   return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
 TEST(add_1, Positive) {
   EXPECT_EQ(1, my_math::add(0, 1));
 }
