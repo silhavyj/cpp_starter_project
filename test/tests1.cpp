29d6a3ea69f36618aaa5a53a1cc42b91d4330ad5
silhavyj <jakub.sil@seznam.cz>
2022-06-24 11:56:54 -0400

    Added a subfolder into src with the name of the project

--- test/tests1.cpp
+++ test/tests1.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
 
-#include "my_math.h"
+#include "myproject/my_math.h"
 
-TEST(add_1, Positive) {
+TEST(add_1, Positive)
+{
     EXPECT_EQ(1, my_math::add(0, 1));
 }
 
-TEST(add_2, Positive) {
+TEST(add_2, Positive)
+{
     EXPECT_EQ(-4, my_math::add(-5, 1));
 }
