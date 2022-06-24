fcb68fad890e12e664ddebc3a563d913cac6ee6a
silhavyj <jakub.sil@seznam.cz>
2022-06-24 12:04:34 -0400

    Changed formatting

--- src/myproject/my_math.cpp
+++ src/myproject/my_math.cpp
@@ -1,16 +1,19 @@
 #include "my_math.h"
 
-namespace my_math {
-
-    int add(int x, int y) {
+namespace my_math
+{
+    int add(int x, int y)
+    {
         return x + y;
     }
 
-    int sub(int x, int y) {
+    int sub(int x, int y)
+    {
         return x - y;
     }
 
-    int mult(int x, int y) {
+    int mult(int x, int y)
+    {
         return x * y;
     }
 }
--- src/myproject/my_math.h
+++ src/myproject/my_math.h
@@ -1,7 +1,7 @@
 #pragma once
 
-namespace my_math {
-
+namespace my_math
+{
     int add(int x, int y);
     int sub(int x, int y);
     int mult(int x, int y);
