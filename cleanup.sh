a056acff05d586aa80c6acb752994f857f8841e2
silhavyj <jakub.sil@seznam.cz>
2022-07-06 11:46:20 -0400

    Added a cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf .vscode             || true
+rm -rf .idea               || true
+rm -rf build               || true 
+rm -rf doc                 || true
+rm -rf cmake-build-release || true
+rm -rf cmake-build-debug   || true
